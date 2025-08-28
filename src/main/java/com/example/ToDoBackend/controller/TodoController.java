0cb6ca1e1ed3090ab48026384ccbdace4b8b47af
HarshAttri47 <161406424+HarshAttri47@users.noreply.github.com>
2025-08-28 16:24:05 +0530

    Initial commit

---
+++ src/main/java/com/example/ToDoBackend/controller/TodoController.java
@@ -0,0 +1,40 @@
+package com.example.ToDoBackend.controller;
+
+import com.example.ToDoBackend.model.Todo;
+import com.example.ToDoBackend.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo todoDetails) {
+        Todo todo = todoRepository.findById(id).orElseThrow();
+        todo.setTitle(todoDetails.getTitle());
+        todo.setCompleted(todoDetails.isCompleted());
+        return todoRepository.save(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoRepository.deleteById(id);
+    }
+}
